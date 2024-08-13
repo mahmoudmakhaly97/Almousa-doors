8150c5fea82dde645d641753c0d82861330ce37f
mahmoud makhaly <mahmoud.abdelfatah@5d-agency.com>
2024-08-13 12:40:42 +0300

    create homepage

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// navbar sticky logic
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector(".navbar");
+  if (window.scrollY >= 58) {
+      navbar.classList.add("fixed-top");
+       navbar.style.background = "#003ea5";
+
+  
+  } else {
+    navbar.classList.remove("fixed-top");
+      navbar.style.background = "transparent";
+  }
+});
+// start our products logic
+$('.products-carousel').owlCarousel({
+   loop:true,
+    margin:10,
+    rtl:true,
+    animateOut: 'fadeOut',
+    autoplay:true,
+
+    navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"], // Replace navigation text with Font Awesome icons
+
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
