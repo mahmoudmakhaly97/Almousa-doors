e98f3b2a2f700b167c84b25bafcf5817a1d2c94c
mahmoud makhaly <mahmoud.abdelfatah@5d-agency.com>
2024-08-14 11:15:58 +0300

    -start creating product page

--- js/main.js
+++ js/main.js
@@ -34,3 +34,45 @@ $('.products-carousel').owlCarousel({
         }
     }
 })
+//active link in navbar
+   document.addEventListener("DOMContentLoaded", function () {
+     const navLinks = document.querySelectorAll(".nav-link");
+
+     const currentUrl = window.location.href;
+
+     navLinks.forEach((link) => {
+       if (link.href === currentUrl) {
+         link.classList.add("active");
+
+         if (link.closest(".nav-dropdown")) {
+           link
+             .closest(".nav-item")
+             .querySelector(".nav-link")
+             .classList.add("active");
+         }
+       }
+     });
+   });
+//  companies section  logic
+$('.companies-carousel').owlCarousel({
+   loop:true,
+    margin:10,
+    rtl:true,
+    animateOut: 'fadeOut',
+    autoplay:true,
+
+    navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"], // Replace navigation text with Font Awesome icons
+
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
